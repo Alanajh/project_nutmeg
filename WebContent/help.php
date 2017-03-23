ba5a8ff2b8cc414f68dd8abefb085a19e8773a50
Alana Harmon <alana.harmon@yahoo.com>
2017-03-22 23:18:00 -0500

    Help screen architecture

--- WebContent/help.php
+++ WebContent/help.php
@@ -1,9 +1,51 @@
 <!DOCTYPE html>
-<html lang="em">
+<html lang="en">
 <head>
-<tltle></title>
+  <title>Nutmeg - help</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+   <script src="javascript/settings.js"></script>
+	<link id="pageStyle" rel="stylesheet" href="css/themes/main/nutmeg.css">
+
 </head>
 <body>
+
+<div class="container-fluid">
+  
+<!-- HELP SCREEN -->
+    
+    <div class="text-center">
+    	<img src="src/nutmeg_logo.png"/><br>
+       </h3>Help</h3>
+       <hr>
+       <br>
+       <h3>Themes</h3>
+       <hr>
+	       <form>
+			  <input type="radio" name="themes" value="default" checked> Default<br>
+			  <input type="radio" name="themes" value="plum"> Plum<br>
+			  <input type="radio" name="themes" value="muted"> Muted<br>
+			  <input type="radio" name="themes" value="powder"> Powder<br>
+			  <input type="radio" name="themes" value="fresh"> Fresh
+			</form>
+       <h3>Tests</h3>
+       <hr>
+       <h3>Room Reach</h3>
+       <hr>
+       <h3>Live</h3>
+       <hr>
+       <h3>Profile</h3>
+       <hr>
+       <h3>Credits</h3>
+    </div>
+</div>  
+
+</body>
+</html>
+
 
 </body>
 </head>
