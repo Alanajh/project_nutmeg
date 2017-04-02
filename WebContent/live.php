77f2a4b2364c81f5df34e2251661b5569c0f4df1
Alana Harmon <alana.harmon@yahoo.com>
2017-04-02 00:49:27 -0500

    live

--- WebContent/live.php
+++ WebContent/live.php
@@ -12,6 +12,49 @@
 <body>
 	<div container="fluid">
 	<!-- left panel -->
+	<div class="bg-info">
+  <div class="container">
+    <div id="carousel-example-vertical" class="carousel vertical slide">
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <p class="ticker-headline">
+            <a href="#">
+            	<button type="button" class="btn btn-primary btn-block">Student 1</button>
+  				<h4>Username</h4>
+            </a>
+          </p>
+        </div>
+        <div class="item">
+          <p class="ticker-headline">
+            <a href="#">
+				<button type="button" class="btn btn-primary btn-block">Student 1</button>
+  				<h4>Username</h4>
+            </a>
+          </p>
+        </div>
+        <div class="item">
+          <p class="ticker-headline">
+            <a href="#">
+				<button type="button" class="btn btn-primary btn-block">Student 1</button>
+  				<h4>Username</h4>
+            </a>
+          </p>
+        </div>
+        
+      </div>
+
+      <!-- Controls -->
+      <a class="up carousel-control" href="#carousel-example-vertical" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-up" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="down carousel-control" href="#carousel-example-vertical" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  </div>
+</div>
 		<div id="leftPanel" class="btn-group-vertical">
 		<table width="90%">
 			<tr>
