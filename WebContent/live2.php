937a56d803a13ece26f87e90b23275bee5c3d7e2
Alana Harmon <alana.harmon@yahoo.com>
2017-04-14 00:24:05 -0500

    GUI 80% dont for LIVE

--- WebContent/live2.php
+++ WebContent/live2.php
@@ -15,47 +15,64 @@
 	<div container="fluid">
 
 <section>
-  <div class="userList">
+ <div class="userList container">
    <!-- USER ONE -->
-		  	<div class="row">
-			  	<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" style="border: solid white 2px;">	
-			  		<div class="row">
-				  		<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-				  		<button type="button" class="btn btn-primary"><input type="image" src="src/users/student_girl_1.PNG" style="width:50px; height:50px; align:left;" >AvaRA</button>
-	  					<h4>Avapants</h4>
-	  					</div>
-	  					<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-	  					<h5>00/00</h5>
-	  					</div>
-  					</div>
-			  	</div>
-		  	</div>
+  	<div class="row">
+		<div class="col-xs-12">
+            <div class="row">
+            	<div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+					<button type="button" class="btn btn-primary"><input type="image" src="src/users/student_girl_1.PNG" style="width:50px; height:50px; align:left;" >AvaRA</button>
+	  				<h4>Avapants</h4>
+				</div>
+                <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2"><h5>00/00</h5></div>
+                <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2"></div>
+            </div>
+        </div>
+	</div>
 		  	
-		  	<!-- USER TWO -->
-		  	<div class="row">
-			  	<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" style="border: solid white 2px;">	
-			  		<button type="button" class="btn btn-primary"><input type="image" src="src/Person.png" style="width:50px; height:50px; align:left;" >AliciaRA</button>
-  					<h4>Pumpkin</h4>
-			  	</div>
-		  	</div>
+  	<!-- USER TWO -->
+	<div class="row">
+		<div class="col-xs-12">
+            <div class="row">
+            	<div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+					<button type="button" class="btn btn-primary"><input type="image" src="src/users/student_girl_1.PNG" style="width:50px; height:50px; align:left;" >AliciaRA</button>
+	  				<h4>Pumpkin</h4>
+				</div>
+                <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2"><h5>00/00</h5></div>
+                <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2"></div>
+            </div>
+        </div>
+	</div>
 		  	
-		  	<!-- USER THREE -->
-		  	<div class="row">
-			  	<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" style="border: solid white 2px;">	
-			  		<button type="button" class="btn btn-primary btn-block"><input type="image" src="src/glasses.png" style="width:50px; height:50px; align:left;" id="userFace">RoseH</button>
-  					<h4>Little Moccassin</h4>
-			  	</div>
-		  	</div>
+  	<!-- USER THREE -->
+	<div class="row">
+		<div class="col-xs-12">
+            <div class="row">
+            	<div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+					<button type="button" class="btn btn-primary"><input type="image" src="src/hipster.png" style="width:50px; height:50px; align:left;" >RosemaryH</button>
+	  				<h4>Little Moccasin</h4>
+				</div>
+                <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2"><h5>00/00</h5></div>
+                <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2"></div>
+            </div>
+        </div>
+	</div>
 		  	
-		  	<!-- USER FOUR -->
-		  	<div class="row">
-			  	<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" style="border: solid white 2px;">	
-			  		<button type="button" class="btn btn-primary btn-block"><input type="image" src="src/hipster.png" style="width:50px; height:50px; align:left;" id="userFace">AlanaJH</button>
-  					<h4>Laney Jo</h4>
-			  	</div>
-		  	</div>
-		  	
-		  	<!-- USER FIVE -->
+	<!-- USER FOUR -->
+	<div class="row">
+			<div class="col-xs-12">
+	            <div class="row">
+	            	<div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+						<button type="button" class="btn btn-primary"><input type="image" src="src/glasses.png" style="width:50px; height:50px; align:left;" >AlanaJH</button>
+		  				<h4>Laney Jo</h4>
+					</div>
+	                <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2"><h5>00/00</h5></div>
+	                <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2"></div>
+	            </div>
+	        </div>
+		</div>		  	
+		
+	<!-- USER FIVE -->
 		  	<div class="row">
 			  	<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" style="border: solid white 2px;">	
 			  		<button type="button" class="btn btn-primary btn-block"><input type="image" src="src/hipster.png" style="width:50px; height:50px; align:left;" id="userFace">AvaRA</button>
@@ -69,18 +86,37 @@
 			  		<button type="button" class="btn btn-primary btn-block"><input type="image" src="src/hipster.png" style="width:50px; height:50px; align:left;" id="userFace">AvaRA</button>
   					<h4>Username</h4>
 			  	</div>
+			  	<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" style="border: solid white 2px;">	
+			  		<button type="button" class="btn btn-primary btn-block"><input type="image" src="src/hipster.png" style="width:50px; height:50px; align:left;" id="userFace">AvaRA</button>
+  					<h4>Username</h4>
+			  	</div>
 		  	</div>
   </div>
   <div class="fixedCont container">
     <div class="row">
         <div id="top" class="col-xs-12">
         	<div class="row">
-        		<div class="col-xs-12"><img id="blink" src="src/Blink-Green.gif"></div>	
+        		<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2"><img id="blink" src="src/Blink-Green.gif">live</div>	
+        		<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6"></div>	
+        		<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" style="text-align: right;">Username<br>Logoff</div>	
+        		<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2"><img id="userLog" src="src/users/student_girl_1.PNG"></div>	
         	</div>
-            <div class="row">
-                <div class="col-xs-2"><div class="">1</div></div>
-                <div class="col-xs-8"><div class="ask">Ask me something<br><button>submit</button></div></div>
-                <div class="col-xs-2"><div class="">3</div></div>
+        	<div class="row">
+        		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"><img id="logo" src="src/nutmeg-800x800.png"><img src="src/nutmeg_logo.png"><br>Tests</div>	
+        	</div>
+            <div id="mid" class="row">
+                <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2"><div class="">advertise</div></div>
+                <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+                	<div  id="details" class="row">Subject: history<br>Title: World Empires</div>
+                	<div class="row">Ask me something<br></div>	
+                	<div class="row"><hr><button>submit</button></div>
+                	<div class="row">Test Result</div>
+                </div>
+                <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2"><div class="">advertise</div></div>
+            </div>
+            <div id="bottom" class="row">
+            	<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">Score: 74%</div>
+                <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">Rank: 13/26</div>
             </div>
         </div>
     </div>
