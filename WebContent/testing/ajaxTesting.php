97f6f0c276ddd08c8f5874488213920f4e7ca3c9
Alana Harmon <alana.harmon@yahoo.com>
2017-05-01 22:19:53 -0500

    .xlsx

--- WebContent/testing/ajaxTesting.php
+++ WebContent/testing/ajaxTesting.php
@@ -1,37 +1,31 @@
 <!DOCTYPE html>
-<head>
-	<title></title>
-
-</head>
+<html>
 <body>
 
-<table id="demo">Core</table>
+<div id='show'></div>
 
 <script>
-function loadXMLDoc() {
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      myFunction(this);
-    }
-  };
-  xmlhttp.open("GET", "http://www.stands4.com/services/v2/abbr.php?uid=5704&tokenid=iaXB5Yu5tb0IRiBA&term=asap", true);
-  xmlhttp.send();
+display(0);
+
+function display(i) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            myFunction(this, i);
+        }
+    };
+    xmlhttp.open("GET", "http://www.stands4.com/services/v2/abbr.php?uid=5704&tokenid=iaXB5Yu5tb0IRiBA&term=, true);
+    xmlhttp.send();
 }
-function myFunction(xml) {
-  var i;
-  var xmlDoc = xml.responseXML;
-  var table="<tr><th>Abbreviation</th><th>Meaning</th></tr>";
-  var x = xmlDoc.getElementsByTagName("result");
-  for (i = 0; i <x.length; i++) { 
-    table += "<tr><td>" +
-    x[i].getElementsByTagName("categoryname")[0].childNodes[0].nodeValue +
-    "</td><td>" +
-    x[i].getElementsByTagName("definition")[0].childNodes[0].nodeValue +
-    "</td></tr>";
-  }
-  document.getElementById("demo").innerHTML = table;
+
+function myFunction(xml, i) {
+    var xmlDoc = xml.responseXML; 
+    x = xmlDoc.getElementsByTagName("result");
+    document.getElementById("show").innerHTML = "Abbreviaton: " +
+    x[i].getElementsByTagName("term")[0].childNodes[0].nodeValue + "<br>Meaning: " +
+    x[i].getElementsByTagName("definition")[0].childNodes[0].nodeValue;
 }
 </script>
+
 </body>
 </html>
