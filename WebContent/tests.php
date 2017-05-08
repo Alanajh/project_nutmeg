66e68fce915e16cb83b46020794f955dc7c42511
Alana Harmon <alana.harmon@yahoo.com>
2017-05-08 00:26:38 -0500

    ---

--- WebContent/tests.php
+++ WebContent/tests.php
@@ -34,7 +34,7 @@ try{
 	$selector = rand(1, 50); // term, definition
 	
     $sql = "SELECT * FROM test_statesCapitals WHERE selector = '$selector' ";   
-    $result = $DBH->query($sql);
+    $result = $DBH->query($selector);
     if($result->rowCount() > 0){
     	
     } else{
